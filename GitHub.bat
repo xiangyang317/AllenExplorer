bd8be4c82a36132221513f530594d3f5c762b8d5
xiangyang317 <xiangyang317@gmail.com>
2023-04-21 22:46:53 +0800

    AllenExplorer

--- GitHub.bat
+++ GitHub.bat
@@ -3,5 +3,4 @@
 "C:\Program Files\Git\bin\git.exe" add .
 "C:\Program Files\Git\bin\git.exe" commit -m "AllenExplorer"
 "C:\Program Files\Git\bin\git.exe" branch -M main
-"C:\Program Files\Git\bin\git.exe" remote add origin https://github.com/xiangyang317/AllenExplorer.git
 "C:\Program Files\Git\bin\git.exe" push -u origin main
