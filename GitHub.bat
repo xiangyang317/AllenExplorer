ecb4c2138b6cce14971a656d71c5f1761bd808d1
xiangyang317 <xiangyang317@gmail.com>
2023-04-21 22:32:19 +0800

    AllenExplorer

---
+++ GitHub.bat
@@ -0,0 +1,6 @@
+"C:\Program Files\Git\bin\git.exe" lfs track .
+"C:\Program Files\Git\bin\git.exe" add .gitattributes
+"C:\Program Files\Git\bin\git.exe" add .
+"C:\Program Files\Git\bin\git.exe" commit -m "AllenExplorer"
+"C:\Program Files\Git\bin\git.exe" branch -M main
+"C:\Program Files\Git\bin\git.exe" push -u origin main
