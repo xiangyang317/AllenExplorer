c78c94cc8ae9513705b4af5e18724eb0433695eb
xiangyang317 <xiangyang317@gmail.com>
2023-11-14 21:03:01 +0800

    AllenExplorer

--- GitHub.bat
+++ GitHub.bat
@@ -1,8 +1,8 @@
-"C:\Program Files\Git\bin\git.exe" lfs track .
-"C:\Program Files\Git\bin\git.exe" add .gitattributes
-"C:\Program Files\Git\bin\git.exe" add .
-"C:\Program Files\Git\bin\git.exe" commit -m "AllenExplorer"
-"C:\Program Files\Git\bin\git.exe" branch -M main
-"C:\Program Files\Git\bin\git.exe" config --global --unset http.proxy
-"C:\Program Files\Git\bin\git.exe" push -u origin main
+git lfs track .
+git add .gitattributes
+git add .
+git commit -m "AllenExplorer"
+git branch -M main
+git config --global --unset http.proxy
+git push -u origin main
 pause
